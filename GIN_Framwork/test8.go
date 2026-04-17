901a3fc980959b8fd777fb0770662de4616c6dea
broken <wangzhezhilu1245@gmail.com>
2026-04-17 09:37:16 +0800

    studying

---
+++ GIN_Framwork/test8.go
@@ -0,0 +1,24 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func distant() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		//检查token,session,cookie等
+		c.Next()
+	}
+}
+func main() {
+	router := gin.Default()
+	public := router.Group("/api")
+	{
+		public.GET("/health")
+	}
+
+	private := router.Group("/api")
+	private.Use(distant())
+	{
+		private.GET("/user")
+	}
+	router.Run(":8080")
+}
---
+++ GIN_Framwork/test9.go
@@ -0,0 +1,28 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+	api := router.Group("/api")
+	{
+		v1 := api.Group("/v1")
+		{
+			users := v1.Group("/users")
+			users.GET("/", func(c *gin.Context) {
+				c.JSON(200, gin.H{
+					"message": "v1 users",
+				})
+			})
+			users.GET("/:id", func(c *gin.Context) {
+				c.JSON(200, gin.H{
+					"message": "v1 user " + c.Param("id"),
+				})
+			})
+
+			id := v1.Group("/id")
+		}
+	}
+
+	router.Run(":8080")
+}
